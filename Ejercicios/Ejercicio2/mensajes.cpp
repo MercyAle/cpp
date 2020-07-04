baf5ba7bb84436f0c9d1690efc3eb9c41e3940cd
Alejandra Paredes <Alejandra-paad@outlook.es>
2020-07-04 03:19:14 -0400

    agregando ejercicio 2

---
+++ Ejercicios/Ejercicio2/mensajes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; 
+
+
+int main(int argc, char const *argv[])
+{
+   string nombre;
+
+   cout << "Ingrese su Nombre: ";
+   cin >> nombre;
+
+   cout << endl; 
+    cout << "Hola mi Nombre es " << nombre ;
+    cout << endl; 
+
+    return 0;
+}
