c1298af6dbd72318d4142b9fc388411939c9e835
Alejandra Paredes <Alejandra-paad@outlook.es>
2020-07-25 17:19:59 -0600

    Agregando ejercicio ciclo for

---
+++ Ejercicios/Ejercicio22-Ciclos For/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    for (int indice = 1; indice <= 10; indice ++)
+    {
+        cout << "Valor de la variable indice " << indice << endl;
+    }
+    
+
+    return 0;
+}
+
+
