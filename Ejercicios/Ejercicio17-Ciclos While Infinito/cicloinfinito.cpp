11e8527045a4402006cc450c092508da64ba3f7c
Alejandra Paredes <Alejandra-paad@outlook.es>
2020-07-25 15:52:41 -0600

    agregando ejercicio ciclo infinito

---
+++ Ejercicios/Ejercicio17-Ciclos While Infinito/cicloinfinito.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{
+    int ciclos = 0;
+    
+
+    while(ciclos < 1000000)
+    {
+        cout << "*";
+
+        ciclos = ciclos + 1;
+    }
+    return 0;
+}
