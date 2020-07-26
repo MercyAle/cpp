ccea0c4c62a9d8eadcec6cac65cb5412e11c42d3
Alejandra Paredes <Alejandra-paad@outlook.es>
2020-07-25 23:36:25 -0600

    Agregando ejercicio de repaso

---
+++ Ejercicios/Ejercicio26-Repaso 1/repaso1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+	
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << "indice:" << i;
+		cout << endl;
+	}
+	
+	cout << endl;
+	cout << endl;
+	
+	int i = 0;
+	
+	while (i <= 10){
+		cout << "indice:" << i;
+		cout << endl;
+		i++;
+	}
+	return 0;
+}
