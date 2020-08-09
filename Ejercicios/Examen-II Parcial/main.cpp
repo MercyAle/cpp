046f646e71d3ba1f799e9566edcc01ec129199f8
Alejandra Paredes <Alejandra-paad@outlook.es>
2020-08-09 17:17:49 -0600

    Agregando Ejercicio de Examen II Parcial

---
+++ Ejercicios/Examen-II Parcial/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>   
+#include<windows.h>
+#include "juego.h" 
+
+using namespace std;
+
+int main()
+{
+	char tecla;
+	int puntos=0;
+	int xPos=30, yPos=20;
+
+	inicializarArreglo();
+	dificultad();	
+	gotoxy(50,2);
+	cout << puntos;
+	pintar();
+	gotoxy(xPos, yPos);
+        cout<<(char)4;	
+
+	while(tecla != 27 && gameover() ){
+	    proceso(tecla, puntos);
+	}
+
+	if ( !gameover()){
+		MessageBoxA(NULL,"Has Perdido","Perdedor", MB_OK);
+		system("cls");
+	}
+
+	system("pause>NULL");
+	return 0;
+} 
