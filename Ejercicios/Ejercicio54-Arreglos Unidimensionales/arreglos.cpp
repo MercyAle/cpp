f9261be22373006aef13db3c50e516b5e7c4a77c
Alejandra Paredes <Alejandra-paad@outlook.es>
2020-08-17 13:40:59 -0600

    Agregando Ejercicio Arreglos Unidimencion

---
+++ Ejercicios/Ejercicio54-Arreglos Unidimensionales/arreglos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeros[5];
+     numeros[0] = 10;
+     numeros[1] = 5;
+     numeros[2] = 7;
+     numeros[3] = 50;
+     numeros[4] = 2;
+
+     cout << numeros[0] << endl;
+     cout << numeros[1] << endl;
+     cout << numeros[2] << endl;
+     cout << numeros[3] << endl;
+     cout << numeros[4] << endl;
+    return 0;
+}
